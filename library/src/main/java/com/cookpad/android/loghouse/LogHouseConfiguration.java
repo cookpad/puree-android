1894c60c13c9192afa49af0879a511216c543a7c
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-08-30 20:41:03 +0900

    Enable to set custom gson

--- library/src/main/java/com/cookpad/android/loghouse/LogHouseConfiguration.java
+++ library/src/main/java/com/cookpad/android/loghouse/LogHouseConfiguration.java
@@ -2,26 +2,42 @@ package com.cookpad.android.loghouse;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+
 public class LogHouseConfiguration {
     private Context applicationContext;
-
-    public LogHouseConfiguration(Context applicationContext) {
-        this.applicationContext = applicationContext;
-    }
+    private Gson gson;
 
     public Context getApplicationContext() {
         return applicationContext;
     }
 
+    public Gson getGson() {
+        return gson;
+    }
+
+    public LogHouseConfiguration(Context applicationContext, Gson gson) {
+        this.applicationContext = applicationContext;
+        this.gson = gson;
+    }
+
     public static class Builder {
         private Context applicationContext;
+        private Gson gson = new Gson();
 
         public Builder(Context applicationContext) {
             this.applicationContext = applicationContext;
         }
 
+        public Builder gson(Gson gson) {
+            this.gson = gson;
+            return this;
+        }
+
         public LogHouseConfiguration build() {
-            return new LogHouseConfiguration(applicationContext);
+            return new LogHouseConfiguration(
+                    applicationContext,
+                    gson);
         }
     }
 }
--- library/src/main/java/com/cookpad/android/loghouse/LogHouseManager.java
+++ library/src/main/java/com/cookpad/android/loghouse/LogHouseManager.java
@@ -4,15 +4,16 @@ import com.google.gson.Gson;
 
 public class LogHouseManager {
     private static final String TAG = LogHouseManager.class.getSimpleName();
-    private static final Gson GSON = new Gson();
+    private static Gson gson;
     private static LogHouseDbHelper logHouseStorage;
 
     public static void initialize(LogHouseConfiguration conf) {
+        gson = conf.getGson();
         logHouseStorage = new LogHouseDbHelper(conf.getApplicationContext());
     }
 
     public static void ask(Log log) {
-        String serializedLog = GSON.toJson(log);
+        String serializedLog = gson.toJson(log);
         logHouseStorage.insert(serializedLog);
         flush();
     }
