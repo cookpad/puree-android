9bbc317c6098fd4e81360d5a38e73fd95115b790
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-08-30 14:21:12 +0900

    Create WareHouseManager

--- demo/src/main/java/com/example/warehouse/MainActivity.java
+++ demo/src/main/java/com/example/warehouse/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.cookpad.android.warehouse.WareHouseManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +15,21 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
     }
 
+    private void setupViews() {
+        findViewById(R.id.track_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                trackClickEvent();
+            }
+        });
+    }
+
+    private void trackClickEvent() {
+        WareHouseManager.ask();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ library/src/main/java/com/cookpad/android/warehouse/WareHouseManager.java
@@ -0,0 +1,7 @@
+package com.cookpad.android.warehouse;
+
+public class WareHouseManager {
+
+    public static void ask() {
+    }
+}
