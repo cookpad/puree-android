b8cb5fb15d9c0053010e187910523ef86bbd5b5e
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-02-22 21:42:48 +0900

    Add test for PureeConfiguration

--- puree/src/androidTest/java/com/cookpad/puree/PureeConfigurationTest.java
+++ puree/src/androidTest/java/com/cookpad/puree/PureeConfigurationTest.java
@@ -7,6 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import com.cookpad.puree.outputs.OutputConfiguration;
 import com.cookpad.puree.outputs.PureeOutput;
 
+import org.hamcrest.Matchers;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
@@ -39,12 +40,27 @@ public class PureeConfigurationTest {
     public void build() {
         Context context = InstrumentationRegistry.getContext();
         PureeConfiguration conf = new PureeConfiguration.Builder(context)
-                .source(FooLog.class).filter(new FooFilter()).filter(new BarFilter()).to(new OutFoo())
-                .source(BarLog.class).filters(new FooFilter(), new BarFilter()).to(new OutFoo())
+                .source(FooLog.class).to(new OutFoo())
+                .source(FooLog.class).filters(new FooFilter()).to(new OutFoo())
+                .source(FooLog.class).filter(new FooFilter()).filter(new BarFilter()).to(new OutBar())
+                .source(BarLog.class).filter(new FooFilter()).to(new OutFoo())
                 .build();
 
         Map<Key, List<PureeOutput>> sourceOutputMap = conf.getSourceOutputMap();
         assertThat(sourceOutputMap.size(), is(2));
+
+        {
+            List<PureeOutput> outputs = sourceOutputMap.get(Key.from(FooLog.class));
+            assertThat(outputs.size(), is(3));
+        }
+        {
+            List<PureeOutput> outputs = sourceOutputMap.get(Key.from(BarLog.class));
+            assertThat(outputs.size(), is(1));
+            assertThat(outputs.get(0).getClass().getName(), is(OutFoo.class.getName()));
+            List<PureeFilter> filters = outputs.get(0).getFilters();
+            assertThat(filters.size(), is(1));
+            assertThat(filters.get(0).getClass().getName(), is(FooFilter.class.getName()));
+        }
     }
 
     private static class FooLog extends JsonConvertible {
@@ -68,6 +84,23 @@ public class PureeConfigurationTest {
     }
 
     private static class OutFoo extends PureeOutput {
+        @Override
+        public String type() {
+            return null;
+        }
+
+        @Override
+        public OutputConfiguration configure(OutputConfiguration conf) {
+            return null;
+        }
+
+        @Override
+        public void emit(JSONObject jsonLog) {
+
+        }
+    }
+
+    private static class OutBar extends PureeOutput {
         @Override
         public String type() {
             return null;
