20a6d414ddfa610b29ba1c79e000b72e3a509e76
k24 <k24@critbit.net>
2015-05-09 17:39:26 +0900

    Handle null of JsonStringifier not to throw an exception

--- puree/src/main/java/com/cookpad/puree/JsonConvertible.java
+++ puree/src/main/java/com/cookpad/puree/JsonConvertible.java
@@ -5,6 +5,11 @@ import org.json.JSONObject;
 
 public abstract class JsonConvertible {
     public JSONObject toJson(JsonStringifier jsonStringifier) {
+        if (jsonStringifier == null) {
+            // Override me or set JsonStringifier to PureeConfiguration
+            return new JSONObject();
+        }
+
         try {
             return new JSONObject(jsonStringifier.toJson(this));
         } catch (JSONException e) {
