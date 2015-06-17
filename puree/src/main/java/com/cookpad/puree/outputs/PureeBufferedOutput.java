39a90274f336b3bb3f14ca6172044d570d191b34
FUJI Goro (gfx) <gfuji@cpan.org>
2015-06-17 22:58:53 +0900

    remove an useless wrappper class AsyncRunnableTask
    Use AsyncTask instead.

--- puree/src/main/java/com/cookpad/puree/outputs/PureeBufferedOutput.java
+++ puree/src/main/java/com/cookpad/puree/outputs/PureeBufferedOutput.java
@@ -4,11 +4,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import com.cookpad.puree.async.AsyncResult;
-import com.cookpad.puree.async.AsyncRunnableTask;
 import com.cookpad.puree.internal.RetryableTaskRunner;
 import com.cookpad.puree.storage.PureeStorage;
 import com.cookpad.puree.storage.Records;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -41,13 +41,14 @@ public abstract class PureeBufferedOutput extends PureeOutput {
 
     @Override
     public void receive(final JsonObject jsonLog) {
-        new AsyncRunnableTask() {
+        new AsyncTask<Void, Void, Void>() {
             @Override
-            public void run() {
+            protected Void doInBackground(Void... params) {
                 JsonObject filteredLog = applyFilters(jsonLog);
                 if (filteredLog != null) {
                     storage.insert(type(), filteredLog);
                 }
+                return null;
             }
         }.execute();
 
@@ -56,11 +57,11 @@ public abstract class PureeBufferedOutput extends PureeOutput {
 
     @Override
     public void flush() {
-        new AsyncRunnableTask() {
-
+        new AsyncTask<Void, Void, Void>() {
             @Override
-            public void run() {
+            protected Void doInBackground(Void... params) {
                 flushSync();
+                return null;
             }
         }.execute();
 
