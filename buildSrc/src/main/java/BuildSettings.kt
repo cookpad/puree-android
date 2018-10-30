abb7073c0a4e7af1285a5e85c033df8beb98296e
Fukuo Kadota <fukuo-kadota@cookpad.com>
2018-10-30 16:16:29 +0900

    Make buildSrc and apply kotlin-dsl

---
+++ buildSrc/src/main/java/BuildSettings.kt
@@ -0,0 +1,6 @@
+@Suppress("unused")
+object BuildSettings {
+    const val minSdkVersion = 14
+    const val targetSdkVersion = 28
+    const val compileSdkVersion = 28
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,18 @@
+@Suppress("unused")
+object Libs {
+    const val appCompatV7 = "com.android.support:appcompat-v7:28.0.0"
+
+    const val gson = "com.google.code.gson:gson:2.8.2"
+    const val jsr305 = "com.google.code.findbugs:jsr305:3.0.2"
+}
+
+@Suppress("unused")
+object TestLibs {
+    // test
+    const val junit = "junit:junit:4.12"
+    const val hamcrest = "org.hamcrest:hamcrest-library:1.3"
+    const val robolectricInstrumentation = "com.github.gfx.android.robolectricinstrumentation:robolectric-instrumentation:3.0.6"
+
+    // androidTest
+    const val androidSupportTestRunner = "com.android.support.test:runner:1.0.2"
+}
